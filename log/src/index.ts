0fcb7638e5306e104710cdaf722c8dbe75ded7da
Taylor Jacovich <tejacovich@gmail.com>
2025-02-16 18:12:27 -0500

    Initial changes to allow passing Bearer tokens.

--- log/src/index.ts
+++ log/src/index.ts
@@ -72,8 +72,8 @@ const run = async () => {
   const now = moment().unix();
 
   // Run tests on each required source
-  for (const [name, url] of sources) {
-    const statusChecker = new StatusChecker(name, url, logger);
+  for (const [name, url, Authorization] of sources) {
+    const statusChecker = new StatusChecker(name, url, Authorization, logger);
     const result = await statusChecker.verifyEndpoint();
 
     let report: ReportFile["site"][number]["status"] | undefined = siteResult.get(name);
--- log/src/status.ts
+++ log/src/status.ts
@@ -1,16 +1,29 @@
 import { ActionLogger } from "./github/types";
 import axios, { AxiosError } from "axios";
 
+export interface Headers {
+    header?: {
+        Authorization?: string;
+    }
+  }
+  
+
 export class StatusChecker {
-    constructor(private readonly siteName: string, private readonly healthEndpoint: string,
-        private readonly logger: ActionLogger) {
+    constructor(private readonly siteName: string, private readonly healthEndpoint: string, private readonly authorization: string,
+        private readonly logger: ActionLogger, private headers?: Headers) {
         logger.info(`Created Status Checker for ${siteName}`);
     }
 
     async verifyEndpoint(): Promise<boolean> {
         try {
+            if (this.authorization) {
+                this.headers={'header':{'Authorization': `Bearer ${this.authorization}`}}
+            }
+            else {
+                this.headers={}
+            }
             this.logger.info(`Contacting ${this.siteName}`);
-            const request = await axios.get(this.healthEndpoint);
+            const request = await axios.get(this.healthEndpoint, this.headers);
             this.logger.debug(`Contacted ${this.healthEndpoint} with status ${request.status}`);
             return request.status >= 200 && request.status < 300;
         } catch (err: unknown | Error | AxiosError) {
